72871d8e588b0e8a7afa14cf6fbf8894221885de
Dmitriy <vertyos@mail.ru>
2023-09-24 09:46:45 +0300

    Создал страницу отображающую все блоги с картинками, мигрировал свои таблицы и создал супер юзера

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     '''Таблица постов'''
-    title = models.CharField('Заголовок', max_length=50)
-    descriptions = models.TextField('Текст записи')
-    author = models.CharField('Автор', max_length=25)
-    date = models.DateField('Дата публикации')
-    image = models.FileField('Изображение', upload_to='image_blog')
+    title = models.CharField(verbose_name='Заголовок', max_length=50)
+    descriptions = models.TextField(verbose_name='Текст записи')
+    author = models.CharField(verbose_name='Автор', max_length=25)
+    date = models.DateField(verbose_name='Дата публикации')
+    image = models.FileField(verbose_name='Изображение', upload_to='image_blog')
 
     def __str__(self):
         return f'{self.title}, {self.author}'
